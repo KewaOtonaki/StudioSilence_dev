d0594fc0724b11c91913765a893ffa98ee4df689
Otonaki Kewa <30595535+owken64@users.noreply.github.com>
2025-03-29 01:23:36 +0900

    HTMLファイルをフェッチ

---
+++ SPA_page_view_Hub/src/components/TopPage.vue
@@ -0,0 +1,13 @@
+<template>
+
+  <h1> Studio Silence </h1>
+
+  <ul>
+    <li> Top </li>
+    <li> Profile </li>
+    <li> Activity</li>
+    <li> Contact </li>
+    <li> Link </li>
+  </ul>
+
+</template>
