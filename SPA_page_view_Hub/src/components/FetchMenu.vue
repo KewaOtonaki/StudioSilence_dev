cdb81b65c50114489ca8a937ff0b45e2d7a0d950
Otonaki Kewa <30595535+owken64@users.noreply.github.com>
2025-03-29 02:18:15 +0900

    調整

--- SPA_page_view_Hub/src/components/FetchMenu.vue
+++ SPA_page_view_Hub/src/components/FetchMenu.vue
@@ -1,30 +1,4 @@
 <script>
-   function getBodyContent( text ) {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(text, "text/html");  // HTMLをDOMに変換
-        return doc.body.innerHTML;  
-   }
-
-   async function renderPage( path ) {
-    const data = await fetch(path)
-    const html_data = await data.text();
-    let element = document.getElementById("fetch-area");
-    element.innerHTML = element.innerHTML + getBodyContent(html_data);
-  };
-
-  let mydata = fetch("/pages/index.json")
-             .then( response => response.text() )
-             .then( data => JSON.parse(data) )
-             .then( data => {
-                    data.children.forEach((child, index) => { 
-                        console.log(child.type);
-                        if (child.type == "file") {
-                            console.log("Load page:" + data.path + child.path );
-                            renderPage( data.path + child.path );
-                        }
-                    });
-                }
-             );
 </script>
 
 <template>
@@ -32,7 +6,4 @@
 </template>
 
 <style>
-#fetch-area  h1 {
-     color: red;
-  }
 </style>
